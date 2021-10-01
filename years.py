0b840890f35474a8f9ef86e0bd8322f13a1922a3
Alex <thedifferentalex@gmail.com>
2021-10-01 13:43:42 +0300

    done

--- years.py
+++ years.py
@@ -4,21 +4,26 @@ while flag:
     try:
         year = int(input())
     except ValueError:
-        print('ошибка')
+        print('Ошибка')
     else:
         flag = False
-if year % 4 != 0:
-    print(f'Год {year} является невисокосным годом')
-else:
-    if year % 100 != 0:
-        print(f'Год {year} является високосным годом')
+if 0 < year <= 3200:
+    print('Год', year, 'принят в обработку')
+    if year % 4 != 0:
+        print(f'Год {year} является невисокосным годом')
     else:
-        if year % 400 == 0:
+        if year % 100 != 0:
             print(f'Год {year} является високосным годом')
         else:
-            print(f'Год {year} является невисокосным годом')
-if year % 100 > 0:
-    print(f'Год {year} относится к {(year // 100) + 1} веку')
+            if year % 400 == 0:
+                print(f'Год {year} является високосным годом')
+            else:
+                print(f'Год {year} является невисокосным годом')
+    if year % 100 > 0:
+        print(f'Год {year} относится к {(year // 100) + 1} веку')
+    else:
+        print(f'Год {year} относится к {year // 100} веку')
 else:
-    print(f'Год {year} относится к {year // 100} веку')
+    print('Ошибка')
+
 
